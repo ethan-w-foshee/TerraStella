79569b505718c79aa56353fcdcf1566afdd26be4
ethan-w-foshee <ethan.w.foshee@gmail.com>
2024-07-19 11:32:54 -0500

    Put a font in

--- main/index.js
+++ main/index.js
@@ -5,18 +5,30 @@ var gameData = {
     ore: 0
 }
 
-var hidden = document.getElementsByClassName("hidden")
+// Get random number generator!
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 function grabStick() {
     // He grab-a da stick!
     gameData.stick += 1
     document.getElementById("sticks").innerHTML = "Sticks: " + gameData.stick
+    document.getElementsByClassName("resources")[0].style.display = "flex";
 }
 
 function grabRock() {
     // We have a small chance to find a shiny rock!!
-    gameData.rock += 1
-    document.getElementById("rocks").innerHTML = "Rocks: " + gameData.rock
+    if (getRandomInt(100) + 1 == 20) {
+        gameData.ore += 1
+        document.getElementById("ores").innerHTML = "Ore: " + gameData.ore
+        document.getElementsByClassName("resources")[2].style.display = "flex";
+    }
+    else {
+        gameData.rock += 1
+        document.getElementById("rocks").innerHTML = "Rocks: " + gameData.rock
+        document.getElementsByClassName("resources")[1].style.display = "flex";
+    }
 }
 
 function reset() {
@@ -28,4 +40,8 @@ function reset() {
     }
     document.getElementById("sticks").innerHTML = "Sticks: " + gameData.stick
     document.getElementById("rocks").innerHTML = "Rocks: " + gameData.rock
+    let resources = document.getElementsByClassName("resources")
+    for (let resource of resources) {
+        resource.style.display = "none"
+    }
 }
